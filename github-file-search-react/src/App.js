625d3136da1992f76ef0935c1403be047c3432b7
waleking <huangwaleking@gmail.com>
2020-08-31 20:42:13 -0400

    add searchview

--- github-file-search-react/src/App.js
+++ github-file-search-react/src/App.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import Header from './components/Header';
 import FilesList from './components/FilesList';
+import SearchView from './components/SearchView';
+import {ESCAPE_CODE, HOTKEY_CODE} from './utils/keyCodes';
 import files from './utils/api';
 
 class App extends React.Component {
   state = {
+    isSearchView: false,
     filesList: files
   };
+
+  componentDidMount(){
+    window.addEventListener('keydown', this.handleEvent);
+  }
+
+  componentWillUnmount(){
+    window.removeEventListener('keydown', this.handleEvent);
+  }
+
+  handleEvent = (event) => {
+    const keyCode = event.keyCode || event.which; //event.which?
+
+    switch(keyCode){
+      case HOTKEY_CODE:
+        this.setState( (prevState)=>({
+          isSearchView: true,
+          filesList: prevState.filesList.filter((file) => file.type === "file")
+        }));
+        break;
+      case ESCAPE_CODE:
+        this.setState({isSearchView: false, filesList: files});
+        break;
+      default:
+        break;
+    }
+  }
 
   render() {
     const { counter, filesList } = this.state; //there's no counter right now!
---
+++ github-file-search-react/src/components/SearchView.js
@@ -0,0 +1,32 @@
+import React, {useState, useEffect, useRef} from 'react';
+
+const SearchView = ({onSearch}) => {
+    const [input, setInput] = useState('');//what's useState? React Hooks
+    const inputRef = useRef();
+
+    useEffect(() => {
+        inputRef.current.focus();
+    }, []); // what's useEffect?
+
+    const onInputChange = (event) => {
+        const input = event.target.value;
+        setInput(input); //?
+        onSearch(input); //?
+    }
+
+    return (
+        <div className="search-box">
+            My Repository <span className="slash">/</span>
+            <input 
+             type = "text"
+             name = "input"
+             value = {input}
+             ref = {inputRef}
+             autoComplete="off"
+             onChange = {onInputChange}
+            />
+        </div>
+    );
+};
+
+export default SearchView;
