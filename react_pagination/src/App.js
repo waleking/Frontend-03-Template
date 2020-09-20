5690eee56f23f4aebcff3e577993eda86e1dedd4
waleking <huangwaleking@gmail.com>
2020-09-20 19:43:11 -0400

    init pagination sub-project

---
+++ react_pagination/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>My App</h1>
+    </div>
+  );
+};
+
+export default App;
