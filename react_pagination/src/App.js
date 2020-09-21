722fc97b3dcb10cdef85a44b2633ed6357a3fbae
waleking <huangwaleking@gmail.com>
2020-09-20 22:58:34 -0400

    render part of posts

--- react_pagination/src/App.js
+++ react_pagination/src/App.js
@@ -3,6 +3,7 @@
 import React, {useState, useEffect} from 'react'; 
 import axios from 'axios';
 import './App.css';
+import { Posts } from './components/Posts';
 
 const App = () => {
   const [posts, setPosts] = useState([]);
@@ -23,9 +24,17 @@ const App = () => {
 
   console.log(posts); // why it's printed several times when refreshing the webpage? 
 
+  // Get current posts which range is [indexOfFirstPost, indexOfLastPost). NOTE: the 
+  // indexOfLastPost may be beyond the scope of the array of posts, but the slice function
+  // can handle this situation safely by returning the sub-array [indexOfFirstPost, len(posts))
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
   return (
-    <div className="container">
-      <h1>My App</h1>
+    <div className="container mt-5">
+      <h1 className="text-primary mb-3">My Blog</h1>
+      <Posts posts={currentPosts} loading={loading} />
     </div>
   );
 };
