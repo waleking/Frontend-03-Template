5a04009b735a4bb5ae35135cd12ac0d483c00624
waleking <huangwaleking@gmail.com>
2020-09-20 22:20:10 -0400

    fectch data by using axios

--- react_pagination/src/App.js
+++ react_pagination/src/App.js
@@ -1,7 +1,28 @@
-import React from 'react';
+// useEffect allows to mimic some of the lifecycle methods within a class-based component. 
+// What we want is to fire off the request to get the posts when component mounts. 
+import React, {useState, useEffect} from 'react'; 
+import axios from 'axios';
 import './App.css';
 
 const App = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false); // ? 
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  const fetchPosts = async () => {
+    setLoading(true);
+    const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    setPosts(res.data);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchPosts();// NOTE: cannot use keyword 'await' outside an async function.
+  }, []); // why leave a blank square brackets here? 
+
+  console.log(posts); // why it's printed several times when refreshing the webpage? 
+
   return (
     <div className="container">
       <h1>My App</h1>
