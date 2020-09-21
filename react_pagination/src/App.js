5aeb28d13bff59e11ac942d3feb45f1eace62505
waleking <huangwaleking@gmail.com>
2020-09-20 23:27:02 -0400

    add a static pagination

--- react_pagination/src/App.js
+++ react_pagination/src/App.js
@@ -4,10 +4,11 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import './App.css';
 import { Posts } from './components/Posts';
+import { Pagination } from './components/Pagination';
 
 const App = () => {
   const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(false); // ? 
+  const [loading, setLoading] = useState(false); // show the loading process, if we are waiting for the json data
   const [currentPage, setCurrentPage] = useState(1);
   const [postsPerPage, setPostsPerPage] = useState(10);
 
@@ -35,6 +36,7 @@ const App = () => {
     <div className="container mt-5">
       <h1 className="text-primary mb-3">My Blog</h1>
       <Posts posts={currentPosts} loading={loading} />
+      <Pagination postsPerPage={postsPerPage} totalPosts={posts.length}/>
     </div>
   );
 };
