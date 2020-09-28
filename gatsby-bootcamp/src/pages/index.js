183463da46610ea357263ebea19a471c863d03f4
waleking <huangwaleking@gmail.com>
2020-09-27 21:10:27 -0400

    the first step of using gatsby.js

--- gatsby-bootcamp/src/pages/index.js
+++ gatsby-bootcamp/src/pages/index.js
@@ -1,5 +1,16 @@
 import React from "react"
 
-export default function Home() {
-  return <div>The Great Gatsby Bootcamp!</div>
+// export default function Home() {
+//   return <div>The Great Gatsby Bootcamp!</div>
+// }
+
+const IndexPage = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+            <h2>I'm Weijing, a full-stack developer living in beautiful Michigan. </h2>
+        </div>                        
+    );
 }
+
+export default IndexPage;
