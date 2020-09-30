ed13a61eea570261cd4ee3ac1a097287fbb93645
waleking <huangwaleking@gmail.com>
2020-09-30 13:31:18 -0400

    add more pages in gatsby project

---
+++ gatsby-bootcamp/src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutPage = () => {
+    return (
+        <div>
+           <h1>About Me</h1> 
+           <p>I'm trying to be a full-stack engineer, and learning the Front-end skills on freecodecamp.</p>
+        </div>
+    )
+}
+
+export default AboutPage;
---
+++ gatsby-bootcamp/src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ContactPage = () => {
+    return (
+        <div>
+           <h1>Contact</h1> 
+           <p>Email: huangwaleking@gmail.com</p>
+        </div>
+    )
+}
+
+export default ContactPage;
