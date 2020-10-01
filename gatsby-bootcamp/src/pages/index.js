e0d3b8aaa01a0732686e321b8b66a3d84da853e0
waleking <huangwaleking@gmail.com>
2020-09-30 22:11:08 -0400

    refactoring: move pages/components to components/

--- gatsby-bootcamp/src/pages/index.js
+++ gatsby-bootcamp/src/pages/index.js
@@ -3,7 +3,7 @@ import React from "react"
 // Link is a component for pre-loading the page, so we can load the page instantly by clicking the 
 // hyper link.
 import { Link } from 'gatsby'; 
-import { Layout } from "./components/Layout";
+import { Layout } from "../components/Layout";
 
 const IndexPage = () => { // No matter what's name of the component defined in the index.js file.
     return (
