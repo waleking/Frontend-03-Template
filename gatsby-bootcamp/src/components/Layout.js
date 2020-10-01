c7ff72e67a7628cb5d5d4174cab38740b76d37ba
waleking <huangwaleking@gmail.com>
2020-09-30 22:14:09 -0400

    use index.css

--- gatsby-bootcamp/src/components/Layout.js
+++ gatsby-bootcamp/src/components/Layout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Footer from "./footer";
 import Header from "./header";
+import '../styles/index.css';
 
 export const Layout = (props) => { // TODO, how do props work here?
     return (
