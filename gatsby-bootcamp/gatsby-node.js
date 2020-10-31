0a52729292db3f32d6bfa7a77c65391d60047c65
waleking <huangwaleking@gmail.com>
2020-10-31 02:38:44 -0400

    dynamically generate markdown post pages via Gatsby's createPage API

--- gatsby-bootcamp/gatsby-node.js
+++ gatsby-bootcamp/gatsby-node.js
@@ -13,3 +13,35 @@ module.exports.onCreateNode = ({ node, actions }) => {
    }
 }
   
+module.exports.createPages = async ({ graphql, actions }) => {
+    const { createPage } = actions;
+
+    // 1. Get path to template
+    const blogTemplate = path.resolve(`./src/templates/blog.js`);
+    const response = await graphql(`
+        query{
+            allMarkdownRemark{
+                edges{
+                    node{
+                        fields {
+                            slug
+                        }
+                    }
+                }
+            }
+        }
+    `)
+    response.data.allMarkdownRemark.edges.forEach((edge) => {
+        createPage({
+            component: blogTemplate,
+            path: `/blog/${edge.node.fields.slug}`,
+            context: {
+                slug: edge.node.fields.slug
+            }
+        }) 
+    })
+
+    // 2. Get markdown data
+
+    // 3. Create new page
+}
