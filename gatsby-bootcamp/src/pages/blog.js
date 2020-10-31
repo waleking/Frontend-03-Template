eaeeed5df4e91a38f4778d98d82c0f7a3fcab443
waleking <huangwaleking@gmail.com>
2020-10-31 01:18:27 -0400

    use graphql and gatsby-transformer-remark to get markdown meta data and render them on the blog page

--- gatsby-bootcamp/src/pages/blog.js
+++ gatsby-bootcamp/src/pages/blog.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import { Layout } from '../components/layout';
+import { graphql, useStaticQuery } from 'gatsby';
+
+// Goal: show lists of posts on blog page.
+//
+// 1. Query the title and date for each post in blog component
+// 2. Render an ol on the page
+// 3. Render a li with a nested h2 (title) and p (date) for each post
+//    - "render array of objects in react"
+// 4. Test your work!
 
 const BlogPage = () => {
+    const data = useStaticQuery(graphql`
+        query{
+            allMarkdownRemark{
+                edges{
+                    node{
+                        frontmatter{
+                            title
+                            date
+                        }
+                        html
+                        excerpt
+                    }
+                }
+            }
+        }
+    `);
+    const markdowns = data.allMarkdownRemark.edges;
     return (
         <Layout>
             <h1>Blog</h1>
-            <p>Posts will show up here later on.</p>
+            <ol>
+            {markdowns.map(markdown => (
+                    <li>
+                        <h2>{markdown.node.frontmatter.title}</h2>
+                        <p>{markdown.node.frontmatter.date}</p>
+                    </li>
+                )
+            )}
+            </ol>
+            
         </Layout>
     )
 }
