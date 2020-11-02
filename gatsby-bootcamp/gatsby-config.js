3fbbe6d6cff3eb0387fa3fa4f2bd9c6eeda0640d
waleking <huangwaleking@gmail.com>
2020-11-02 13:35:26 -0500

    add image, and render image in md files

--- gatsby-bootcamp/gatsby-config.js
+++ gatsby-bootcamp/gatsby-config.js
@@ -5,30 +5,45 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  /* Visit the data stored in siteMetadata, we can use GraphQL query: */
-  /* query {
-    site {
-      siteMetadata {
-        title
-        description
-        author
+    /* Your site config here */
+    /* Visit the data stored in siteMetadata, we can use GraphQL query: */
+    /* query {
+      site {
+        siteMetadata {
+          title
+          description
+          author
+        }
       }
-    }
-  } */
-  siteMetadata: { // It's the field that we want to show how to be visited by GraphQL
-    title: 'Full-Stack Bootcamp!',
-    author: 'Weijing Huang'
-  },
-  plugins: [
-      'gatsby-plugin-sass',
-      {
-          resolve: 'gatsby-source-filesystem',
-          options: {
-              name: "src",
-              path: `${__dirname}/src/`
-          }
-      },
-      'gatsby-transformer-remark'
-  ],
+    } */
+    siteMetadata: { // It's the field that we want to show how to be visited by GraphQL
+        title: 'Full-Stack Bootcamp!',
+        author: 'Weijing Huang'
+    },
+    plugins: [
+        'gatsby-plugin-sass',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: "src",
+                path: `${__dirname}/src/`
+            }
+        },
+        'gatsby-plugin-sharp', // Exposes several image processing functions built on the Sharp image processing library. 
+        {
+            resolve: 'gatsby-transformer-remark',
+            options: {
+                plugins: [
+                    'gatsby-remark-relative-images',
+                    {
+                        resolve: 'gatsby-remark-images',
+                        options: {
+                            maxWidth: 750,
+                            linkImagesToOriginal: false
+                        }
+                    }
+                ]
+            }
+        }
+    ],
 }
--- gatsby-bootcamp/src/posts/gatsby/gatsby.md
+++ gatsby-bootcamp/src/posts/gatsby/gatsby.md
@@ -5,6 +5,8 @@ date: "2020-10-21"
 
 I just launched a new bootcamp!
 
+![Grass](./grass.jpg)
+
 ## Topics Covered
 1. Gatsby
 2. GraphQL
